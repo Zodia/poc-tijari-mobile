824be22f07d424c6cfe8ce21d968a529b2577819
Abssi <abssi.casanearshore.ma.ibm.com>
2014-10-14 09:28:53 +0100

    new js to prefill form

--- attijari-poc-mobile-app/apps/attijari_poc_mobile_app/common/js/pages/customer-view.js
+++ attijari-poc-mobile-app/apps/attijari_poc_mobile_app/common/js/pages/customer-view.js
@@ -13,3 +13,48 @@ $(".current-title").text ("Customer View");
 $(".left").on ("click", function () {
 	loadPage ("pages/customer-view.html"); // "pages/splash-view.html"
 });
+
+
+
+autoFill{
+
+data = {
+	        "Title": "Mr",
+	        "First_name": "Rachid",
+	        "Last_name": "Mahmoudi",
+	        "Nationality": "Morrocan",
+	        "Email": "az@gmail.com",
+	        "Mobile":"(212)-6757657 676545",
+	        "Monthly_income":"60000",
+	        "personal_loans":"Yes",
+	        "MP_personal_loans":"3000",
+	        "car_loans":"Yes",
+	        "MP_car_loans":"2000",
+	        "existing_mortgages":"Yes",
+	        "MP_existing_mortgages":"4000",
+	        "Existing_debit_card":"Yes",
+	        "Existing_credit_card":"No"
+	       
+	        	
+	};
+
+	// reset form values from json object
+	function resetForm(){ 
+		$.each(data, function (name, val) {
+	    var $el = $('[name="' + name + '"]'),
+	        type = $el.attr('type');
+
+	    switch (type) {
+	        case 'checkbox':
+	            $el.attr('checked', 'checked');
+	            break;
+	        case 'radio':
+	            $el.filter('[value="' + val + '"]').attr('checked', 'checked');
+	            break;
+	        default:
+	            $el.val(val);
+	    }
+	})}
+}
+	
+ resetForm();
