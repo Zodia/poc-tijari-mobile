08900c3fc11032579c76de2ab248f18c30ee46e1
ADMINIBM <ADMINIBM@ADMINIB-G03T92A.casanearshore.ma.ibm.com>
2014-10-14 17:42:34 +0100

    Adding Shared Variable to the main.js

--- attijari-poc-mobile-app/apps/attijari_poc_mobile_app/common/js/pages/customer-entry-view.js
+++ attijari-poc-mobile-app/apps/attijari_poc_mobile_app/common/js/pages/customer-entry-view.js
@@ -14,7 +14,15 @@ $("#existinguser").click(function() {
 	$("#listholder").show();	
 	$("#newuser").hide();
 	$("#existinguser").hide();
+	
 });
+
+$('#listview').children('li').on('click', function () {
+	  // alert('Selected Name=' + $(this).find("id").text());
+	   customer_index =$(this).attr("id");
+	   loadPage("pages/customer-view.html");
+	});
+
 
 
 $("#newuser").on ("click", function () {
