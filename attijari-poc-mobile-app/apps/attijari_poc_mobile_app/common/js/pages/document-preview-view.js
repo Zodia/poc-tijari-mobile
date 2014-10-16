43a558e8732a99020a7c2874db3e1578bd93377f
Zodia <mehdiben@ma.ibm.com>
2014-10-16 11:04:16 +0100

    document preview view update

--- attijari-poc-mobile-app/apps/attijari_poc_mobile_app/common/js/pages/document-preview-view.js
+++ attijari-poc-mobile-app/apps/attijari_poc_mobile_app/common/js/pages/document-preview-view.js
@@ -1,7 +1,3 @@
 $(".left .back").text("Back");
 
-$(".left").unbind("click").on("click", function() {
-	loadPage("pages/supporting-docs-view.html"); // "pages/splash-view.html"
-});
-
 $(".current-title").text ("Documents Viewer");
---
+++ attijari-poc-mobile-app/apps/attijari_poc_mobile_app/ipad/native/www/default/js/pages/document-preview-view.js
@@ -0,0 +1,5 @@
+
+/* JavaScript content from js/pages/document-preview-view.js in folder common */
+$(".left .back").text("Back");
+
+$(".current-title").text ("Documents Viewer");
