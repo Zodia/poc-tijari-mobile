2de79f96ca48acb513cf4e8854781f0b379cca33
Zodia <mehdiben@ma.ibm.com>
2014-10-17 15:20:07 +0100

    generalizing the confirmation page

--- attijari-poc-mobile-app/apps/attijari_poc_mobile_app/common/js/pages/submit-problem-view.js
+++ attijari-poc-mobile-app/apps/attijari_poc_mobile_app/common/js/pages/submit-problem-view.js
@@ -1,5 +1,36 @@
 $(".current-title").text ("Submit problem details");
 
 $(".send-problem-button"). on ("click", function () {
-	loadPage ("pages/confirmation-view.html");
+	
+	var message = "Thank you! Problem details have been successfully submitted!";
+	/*var additionalAction = '<a href="#" data-role="button" data-inline="true" data-theme="b" id="loginAction">New application</a>';
+	var promoteTitle = "Next best offers";
+	var promoteContent = '<div class="ui-grid-a">';
+		promoteContent += '<div class="ui-block-a">';
+		promoteContent += '<div class="ui-bar ui-bar-e">'
+		promoteContent += '<i class="fa fa-car"></i>';
+		promoteContent += '<p>Buy a car</p>';
+		promoteContent += '</div>';
+		promoteContent += '</div>';
+		promoteContent += '<div class="ui-block-b">';
+		promoteContent += '<div class="ui-bar ui-bar-e">'
+		promoteContent += '<i class="fa fa-home"></i>';
+		promoteContent += '<p>Buy a house</p>';
+		promoteContent += '</div>';
+		promoteContent += '</div>';
+		promoteContent += '<div class="ui-block-a">';
+		promoteContent += '<div class="ui-bar ui-bar-e">'
+		promoteContent += '<i class="fa fa-plane"></i>';
+		promoteContent += '<p>Take a vacation</p>';
+		promoteContent += '</div>';
+		promoteContent += '</div>';
+		promoteContent += '<div class="ui-block-b">';
+		promoteContent += '<div class="ui-bar ui-bar-e">'
+		promoteContent += '<i class="fa fa-university"></i>';
+		promoteContent += '<p>Go to college</p>';
+		promoteContent += '</div>';
+		promoteContent += '</div>';
+		promoteContent += '</div>';*/
+	
+	confirmOperation (message, null, null, null);
 });
