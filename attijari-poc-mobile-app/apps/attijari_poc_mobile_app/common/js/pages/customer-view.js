4d78c9c0fb9676ca0b3ee888e0a2f3bd46ca7751
Abssi <abssi.casanearshore.ma.ibm.com>
2014-10-20 16:31:40 +0100

    adding the photography and documents sections

--- attijari-poc-mobile-app/apps/attijari_poc_mobile_app/common/js/pages/customer-view.js
+++ attijari-poc-mobile-app/apps/attijari_poc_mobile_app/common/js/pages/customer-view.js
@@ -93,6 +93,10 @@ function resetForm() {
 
 resetForm();
 
+//photography preview
+
+
+
 
 $(document).ready(function(){
 
@@ -106,6 +110,10 @@ $(document).ready(function(){
         dateFormat :'dd/mm/yy',
         theme: 'ios'
     });
+	
+	// photography preview
+	
+	
 	
 	
 	
@@ -125,3 +133,51 @@ function hideHistory(){
 
 
 hideHistory(); 
+
+
+///////////////////////
+
+function displayAsImage3(file, containerid) {
+	if (typeof FileReader !== "undefined") {
+	  
+		var container = document.getElementById(containerid),
+		    img = document.createElement("img"),
+		    reader;
+		container.appendChild(img);
+		reader = new FileReader();
+		reader.onload = (function (theImg) {
+			return function (evt) {
+				theImg.src = evt.target.result;
+			};
+		}(img));
+		reader.readAsDataURL(file);
+	}
+}
+
+$("#chooseFile").click(function(e){
+	e.preventDefault();
+	$("#imageInput").trigger("click");
+});
+$("#imageInput").change(function(){
+	var file = $("#imageInput")[0].files[0];            
+	$("#preview").empty();
+	displayAsImage3(file, "preview");
+	
+	$info = $("#info");
+	$info.empty();
+	if (file && file.name) {
+		$info.append("<li>name:<span>" + file.name + "</span></li>");
+	}
+	if (file && file.type) {
+		$info.append("<li>size:<span>" + file.type + " bytes</span></li>");
+	}
+	if (file && file.size) {
+		$info.append("<li>size:<span>" + file.size + " bytes</span></li>");
+	}
+	if (file && file.lastModifiedDate) {
+		$info.append("<li>lastModifiedDate:<span>" + file.lastModifiedDate + " bytes</span></li>");
+	}
+	$info.listview("refresh");
+});
+
+
