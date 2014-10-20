8d73bb2ba96f85859ad67a68285602827a59365a
shaillum <Shailendra@ShailendraM_PC.ae.ibm.com>
2014-10-20 17:23:17 +0400

    Ask an Expert : Validation for Expert status (Busy & Offline)

--- attijari-poc-mobile-app/apps/attijari_poc_mobile_app/common/js/pages/ask_expert/expert-view.js
+++ attijari-poc-mobile-app/apps/attijari_poc_mobile_app/common/js/pages/ask_expert/expert-view.js
@@ -1,27 +1,49 @@
-
 // Invoking "Chat with expert" page
 /*	$("#carLoanExpert1").on ("click", function () {
-		//$("#main-chat-with-expert").style.visibility = 'visible';
-		loadPage ("pages/ask_expert/chat-with-expert.html");
-	});
-	
-	// Invoking "Chat with expert" page
-	$("#searchlistitem1").on ("click", function () {
-		loadPage ("pages/ask_expert/chat-with-expert.html");
-	});*/
-	
+ //$("#main-chat-with-expert").style.visibility = 'visible';
+ loadPage ("pages/ask_expert/chat-with-expert.html");
+ });
+
+ // Invoking "Chat with expert" page
+ $("#searchlistitem1").on ("click", function () {
+ loadPage ("pages/ask_expert/chat-with-expert.html");
+ });*/
 
 // Chat with Expert in chat window
-function chatWithExpert(expertFirstName,expertLastName, expertType){
-	
-	//$("#chatWindowSection").style.display='visible';
-	//$("#chatWindowSection").hide();
+function chatWithExpert(expertFirstName, expertLastName, expertType) {
+
+	// $("#chatWindowSection").style.display='visible';
+	// $("#chatWindowSection").hide();
 	$("#chatWindowSection").show();
-	
-	$("#expertImageSection").html('<img src="images/pages/ask_expert/chat_with_expert/expert-'+expertFirstName+'.jpg">');
-	$("#expertInfoSection").html('<div class="ui-block-b" id="expertInfoSection" style="width: 65%">'
-			+'<font face="verdana" size="4" color="white">'+expertFirstName+' '+expertLastName
-			+'</font><br> <font face="verdana" size="1" color="white">Designation:'+expertType
-			+'<br> Phone: +971 566766488<br>Email:'+expertFirstName+'.'+expertLastName+'@ae.ibm.com' 
-			+'<br> Address: Building 5,	Rabat, Morocco</font>	');
-	};
+
+	$("#expertImageSection").html(
+			'<img src="images/pages/ask_expert/chat_with_expert/expert-'
+					+ expertFirstName + '.jpg">');
+	$("#expertInfoSection")
+			.html(
+					'<div class="ui-block-b" id="expertInfoSection" style="width: 65%">'
+							+ '<font face="verdana" size="4" color="white">'
+							+ expertFirstName
+							+ ' '
+							+ expertLastName
+							+ '</font><br> <font face="verdana" size="1" color="white">Designation:'
+							+ expertType
+							+ '<br> Phone: +971 566766488<br>Email:'
+							+ expertFirstName
+							+ '.'
+							+ expertLastName
+							+ '@ae.ibm.com'
+							+ '<br> Address: Building 5,	Rabat, Morocco</font>	');
+};
+
+// Alert messages when experts are Busy & Offline
+function alertMsgPopup(status) {
+
+	$("#chatWindowSection").hide();
+	$("#statusAlertPopUp")
+			.html(
+					'<img src="images/pages/ask_expert/expert_view/icon-alert.png">'
+							+ '<a href="#" data-rel="back" class="ui-btn ui-corner-all ui-shadow ui-btn-a ui-icon-delete ui-btn-icon-notext ui-btn-right">Close</a> '
+							+ 'Currently expert is ' + status
+							+ ', Please reach out to other experts');				
+     }
