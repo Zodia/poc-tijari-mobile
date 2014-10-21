3477ccd21ee39209484e037b639f78fa2c2893ca
Abssi <abssi.casanearshore.ma.ibm.com>
2014-10-21 14:46:54 +0100

    adding mortgage-file-choice.js

---
+++ attijari-poc-mobile-app/apps/attijari_poc_mobile_app/common/js/pages/mortgage-file-choice-view.js
@@ -0,0 +1,26 @@
+$("#yes").click(function() {
+
+$("#pageContent").load("pages/current-terms.html", function () {
+		
+		$(this).enhanceWithin(); /* apply styles */
+	});
+	
+});
+
+
+
+
+$("#no").on ("click", function () {
+	customer_index = -1;
+	$("#pageContent").load("pages/current-terms.html", function () {
+		
+		$(this).enhanceWithin(); /* apply styles */
+	});
+});
+
+$( document ).ready(function() {
+	$(".left .back").text("Back");
+	
+	$(".current-title").text ("Mortgage File");	
+	
+});
