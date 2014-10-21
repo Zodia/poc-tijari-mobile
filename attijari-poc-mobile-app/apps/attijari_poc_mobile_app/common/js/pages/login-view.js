8268b624298db4a6fa5d37464635483dc16f0d33
Zodia <mehdiben@ma.ibm.com>
2014-10-21 18:21:11 +0100

    login page fixed

--- attijari-poc-mobile-app/apps/attijari_poc_mobile_app/common/js/pages/login-view.js
+++ attijari-poc-mobile-app/apps/attijari_poc_mobile_app/common/js/pages/login-view.js
@@ -1,62 +1,37 @@
 $( document ).ready(function() {
 	$(".current-title").text ("Login Page");
-	$("#dialog").hide();	
-	$(".right").hide();
-	$("#header").show();
-	$("#footer").hide();
-	$("#dialog").click(function () {
-        //$(this).dialog('close');
-    	$("#dialog").hide();
+	$("#dialog").hide ();	
+	$(".right").hide ();
+	$("#header").show ();
+	$("#footer").hide ();
+	$("#dialog").click (function () {
+        $("#dialog").hide ();
     });
-	if (connected == 1){
-		
-
-		loadPage("pages/apps-list.html");
-			
-	}
-			
+	
+	if (connected == 1) {
+		loadPage("pages/apps-list.html");		
+	}	
 });
 
-
-
-$("#forgotpass").on("click",function(){
-	
-	$("#pageContent").load("pages/forgot-password-view.html", function () {
-		$(this).enhanceWithin();
-	});
+$("#forgotpass").on ("click",function () {
+	loadPage ("pages/forgot-password-view.html");
 });
 
-
-
-$("#gosemulator").on("click",function(){
-	
-	$("#pageContent").load("pages/mortgage-simulator-view.html", function () {
-		$(this).enhanceWithin();
-	});
+$("#gosemulator").on ("click",function () {
+	loadPage ("pages/mortgage-simulator-view.html");
 });
 
 $("#buttongo").on("click",function(){
-  //alert(login);
-	if($('#login').val()=="demo" && $('#password').val()=="demo"  )
-	{
-		
+	if($('#login').val () == "demo" && $('#password').val () == "demo") {
 		connected = 1;
-		$(".right").show();
-		$("#footer").show();	
+		$(".right").show ();
+		$("#footer").show ();	
 	
 		//loadPage("pages/home-view.html");
-		loadPage("pages/home-view.html");
-		
-		
-		
+		loadPage ("pages/home-view.html");	
+	} else {
+		$("#dialog").dialog ();
+		$("#dialog").show ();
 	}
-	else
-	{
-		
-	
-		$( "#dialog").dialog();
-		$("#dialog").show();
-		
-	}
-});
-
+}); 
+//
