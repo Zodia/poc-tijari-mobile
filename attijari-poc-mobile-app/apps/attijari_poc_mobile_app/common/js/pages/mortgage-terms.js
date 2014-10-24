5a9e9b257d272d90e7fc866aba63e98c310b66e9
ADMINIBM <ADMINIBM@ADMINIB-G03T92A.casanearshore.ma.ibm.com>
2014-10-24 19:17:56 +0100

    Confirmation Update

--- attijari-poc-mobile-app/apps/attijari_poc_mobile_app/common/js/pages/mortgage-terms.js
+++ attijari-poc-mobile-app/apps/attijari_poc_mobile_app/common/js/pages/mortgage-terms.js
@@ -1,6 +1,90 @@
 $("#back").on("click", function() {
 	loadPage("pages/property-view.html");
 });
-$("#submit").on("click", function() {
-	alert("Submited");
-});
+function confirmDialog(text, callback) {
+	var popupDialogId = 'popupDialog';
+	$(
+			'<div data-role="popup" id="'
+					+ popupDialogId
+					+ '" data-confirmed="no" data-transition="pop" data-overlay-theme="b" data-theme="b" data-dismissible="false" style="max-width:500px;"> \
+	                    <div data-role="header" data-theme="a">\
+	                        <h1>Are you sure?</h1>\
+	                    </div>\
+	                    <div role="main" class="ui-content" align="center">\
+	                        <h3 style="color:black" class="ui-title">'
+					+ text
+					+ '<div class="ui-grid-a ui-responsive" id="button-div">'
+						+'<div class="ui-block-a"><a href="#" id="btn-back" class=" optionConfirm" data-rel="back">Yes</a></div>'
+						+'<div class="ui-block-b"><a href="#" id="btn-submit" class="btn-submit optionCancel" data-rel="back" data-transition="flow" >No</a></div>'
+					+'</div>'
+	                +'</div>')
+			.appendTo($.mobile.pageContainer);
+	var popupDialogObj = $('#' + popupDialogId);
+	popupDialogObj.trigger('create');
+	popupDialogObj
+			.popup({
+				afterclose : function(event, ui) {
+					popupDialogObj.find(".optionConfirm").first().off('click');
+					var isConfirmed = popupDialogObj.attr('data-confirmed') === 'yes' ? true
+							: false;
+					$(event.target).remove();
+					if (isConfirmed && callback) {
+						callback();
+					}
+				}
+			});
+	popupDialogObj.popup('open');
+	popupDialogObj.find(".optionConfirm").first().on('click', function() {
+		popupDialogObj.attr('data-confirmed', 'yes');
+	});
+}
+
+$('#submit')
+		.on(
+				'click',
+				function() {
+					confirmDialog(
+							"submit",
+							function() {
+								var message = "Thank you for your mortgage application."
+										+ "Our Mortgage Specialists will analyse it and will be"
+										+ " responding to your application soon.";
+								var additionalAction = '<a data-role="button" data-inline="true" data-theme="b" id="new-app-action">New application</a>';
+								var promoteTitle = "Next best offers";
+								var promoteContent = '<div class="ui-grid-a">';
+								promoteContent += '<div class="ui-block-a">';
+								promoteContent += '<div class="ui-bar ui-bar-e">'
+								promoteContent += '<i class="fa fa-car"></i>';
+								promoteContent += '<p>Buy a car</p>';
+								promoteContent += '</div>';
+								promoteContent += '</div>';
+								promoteContent += '<div class="ui-block-b">';
+								promoteContent += '<div class="ui-bar ui-bar-e">'
+								promoteContent += '<i class="fa fa-home"></i>';
+								promoteContent += '<p>Buy a house</p>';
+								promoteContent += '</div>';
+								promoteContent += '</div>';
+								promoteContent += '<div class="ui-block-a">';
+								promoteContent += '<div class="ui-bar ui-bar-e">'
+								promoteContent += '<i class="fa fa-plane"></i>';
+								promoteContent += '<p>Take a vacation</p>';
+								promoteContent += '</div>';
+								promoteContent += '</div>';
+								promoteContent += '<div class="ui-block-b">';
+								promoteContent += '<div class="ui-bar ui-bar-e">'
+								promoteContent += '<i class="fa fa-university"></i>';
+								promoteContent += '<p>Go to college</p>';
+								promoteContent += '</div>';
+								promoteContent += '</div>';
+								promoteContent += '</div>';
+
+								confirmOperation(message, additionalAction,
+										promoteTitle, promoteContent);
+								$("#confirmation-wrapper .confirmation-content .confirmation-message .additional-action #new-app-action")
+										.on(
+												"click",
+												function() {
+													loadPage("pages/mortgage-center-view.html");
+												});
+							});
+				});
