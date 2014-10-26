03dd7a4df8afd56994c3d3967f8f81e154adb183
Zodia <mehdiben@ma.ibm.com>
2014-10-26 17:15:04 +0100

    login-view js update

--- attijari-poc-mobile-app/apps/attijari_poc_mobile_app/common/js/pages/login-view.js
+++ attijari-poc-mobile-app/apps/attijari_poc_mobile_app/common/js/pages/login-view.js
@@ -1,34 +1,28 @@
 $( document ).ready(function() {
-	$(".current-title").text ("Login Page");
 	$("#dialog").hide ();	
-	$(".right").hide ();
-	$("#header").show ();
-	$("#footer").hide ();
 	$("#dialog").click (function () {
         $("#dialog").hide ();
     });
 	
 	if (connected == 1) {
-		loadPage("pages/apps-list.html");		
+		loadPage("apps-list.html");		
 	}	
 });
 
 $("#forgotpass").on ("click",function () {
-	loadPage ("pages/forgot-password-view.html");
+	loadPage ("forgot-password-view.html");
 });
 
 $("#registerlink").on ("click",function () {
-	loadPage ("pages/register-view.html");
+	loadPage ("register-view.html");
 });
 
 $("#buttongo").on("click",function(){
 	if($('#emailinput').val () == "demo" && $('#passwordinput').val () == "demo") {
 		connected = 1;
-		$(".right").show ();
-		$("#footer").show ();	
 	
 		//loadPage("pages/home-view.html");
-		loadPage ("pages/home-view.html");	
+		loadPage ("home-view.html");	
 	} else {
 		$("#dialog").dialog ();
 		$("#dialog").show ();
