838db75c8ea4d5b5482a65206cd1ebd4947349c6
next1 <h_maazouz@hotmail.com>
2014-10-27 14:25:36 +0000

    adding arabic  translation to the  login view

--- attijari-poc-mobile-app/apps/attijari_poc_mobile_app/common/js/pages/mortgage-center-view.js
+++ attijari-poc-mobile-app/apps/attijari_poc_mobile_app/common/js/pages/mortgage-center-view.js
@@ -2,8 +2,6 @@
  * 
  */
 
-
-
 $("#existingapp").click(function() {
 
 loadPage("../pages/apps-list-view.html");
@@ -23,44 +21,7 @@ $("#mortgagesimulator").click(function() {
 	loadPage("../pages/mortgage-simulator-view.html");
 		
 	});
-//$("#newapp").on ("click", function () {
-//	customer_index = -1;
-//	$("#pageContent").load("pages/customer-entry-view.html", function () {
-//		
-//		$(this).enhanceWithin(); /* apply styles */
-//	});
-//});
 
-
-
-/*
- * starting  the slide  effect
- * */
-$("#one").hide();
-$("#two").hide();
-$("#tree").hide();
-var i=0;
-$("#slider").on ("click", function () {
-	//$("#two").show("slide", { direction: "right" }, 1200);
-	//$( "#slider" ).hide();
-	//$( "#slider" ).html( $("#two").html());
-	if(i==0){
-		$( "#slider" ).html( $("#one").html());
-		//$("#one").show("slide", { direction: "left" }, 3000);
-	i=i+1;
-	return -1;
-	}
-	if(i==1){
-		$( "#slider" ).html( $("#two").html());
-		i=i+1;
-		return -1; 
-		}
-	if(i==3){
-		$( "#slider" ).html( $("#tree").html());
-		i=0;
-		return -1;
-		}
-});
 
 
 $("#mortgages").click(function() {
@@ -69,6 +30,3 @@ loadPage("../spages/mortgage-center-view.html");
 
 		
 });
-
-
-
