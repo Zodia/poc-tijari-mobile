3c78b758c568ea279987ddeafe0462d23d3a86de
next1 <h_maazouz@hotmail.com>
2014-10-29 20:16:36 +0000

    mortgage center:  slide effect

--- attijari-poc-mobile-app/apps/attijari_poc_mobile_app/common/js/pages/mortgage-center-view.js
+++ attijari-poc-mobile-app/apps/attijari_poc_mobile_app/common/js/pages/mortgage-center-view.js
@@ -1,3 +1,43 @@
+$( document ).ready(function() {
+	var options = {
+			$AutoPlay: "false",
+			$AutoPlayInterval: 100000,
+            $Loop: 0,            
+            $DragOrientation: 3, 
+            $BulletNavigatorOptions: {                                //[Optional] Options to specify and enable navigator or not
+                $Class: $JssorBulletNavigator$,                       //[Required] Class to create navigator instance
+                $ChanceToShow: 2,                               //[Required] 0 Never, 1 Mouse Over, 2 Always
+                $AutoCenter: 1,                                 //[Optional] Auto center navigator in parent container, 0 None, 1 Horizontal, 2 Vertical, 3 Both, default value is 0
+                $Steps: 1,                                      //[Optional] Steps to go for each navigation request, default value is 1
+                $Lanes: 1,                                      //[Optional] Specify lanes to arrange items, default value is 1
+                $SpacingX: 8,                                   //[Optional] Horizontal space between each item in pixel, default value is 0
+                $SpacingY: 8,                                   //[Optional] Vertical space between each item in pixel, default value is 0
+                $Orientation: 1                                 //[Optional] The orientation of the navigator, 1 horizontal, 2 vertical, default value is 1
+            }
+        };
+	var options1 = {
+			$Transitions :{$Duration:1200,x:-0.3,y:0.3,$Delay:60,$Zoom:1,$Formation:$JssorSlideshowFormations$.$FormationStraightStairs,$Easing:{$Left:$JssorEasing$.$EaseInJump,$Top:$JssorEasing$.$EaseInJump,$Opacity:$JssorEasing$.$EaseLinear},$Opacity:2,$Round:{$Left:0.8,$Top:0.8}},
+			$AutoPlayInterval: 5000,
+			$AutoPlay: true,
+			$DragOrientation: 3,                               
+	};
+	
+
+		var jssor_slider1 = new $JssorSlider$("slideshow5", options);
+		var jssor_slider1 = new $JssorSlider$("slideshow6", options);
+		var jssor_slider1 = new $JssorSlider$("slideshow7", options);
+		var jssor_slider1 = new $JssorSlider$("slideshow8", options);
+		var jssor_slider1 = new $JssorSlider$("slideshow9", options);
+
+		
+   
+        jssor_slider1.$Play();
+	
+});
+
+
+
+
 $(".applications").click(function() {
 	loadPage("apps-list-view.html");
 });
