e3f39dfd688dab54b4c78385e10c463c2ce5bfc5
Mehdi <mehdiben@ma.ibm.com>
2014-10-31 16:23:35 +0000

    Update applications-search-view.js

--- attijari-poc-mobile-app/apps/attijari_poc_mobile_app/common/js/pages/applications-search-view.js
+++ attijari-poc-mobile-app/apps/attijari_poc_mobile_app/common/js/pages/applications-search-view.js
@@ -1,14 +1,4 @@
-/**
- * 
- */
-
 $('#listview').children('li').on('click', function () {
-	  // alert('Selected Name=' + $(this).find("id").text());
 	application_index =$(this).attr("id"); 
-	   loadPage("application-details.html");
-	});
-
-$( document ).ready(function() {
-	$(".left .back").text("Back");
+	loadPage("application-details.html");
 });
-
