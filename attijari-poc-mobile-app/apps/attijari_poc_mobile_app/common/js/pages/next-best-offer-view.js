53a79b7e9a4ec7b919615e561dbfa47cf4055ea8
Mehdi <mehdiben@ma.ibm.com>
2014-10-31 16:25:48 +0000

    Update next-best-offer-view.js

--- attijari-poc-mobile-app/apps/attijari_poc_mobile_app/common/js/pages/next-best-offer-view.js
+++ attijari-poc-mobile-app/apps/attijari_poc_mobile_app/common/js/pages/next-best-offer-view.js
@@ -1,16 +1,11 @@
 $(document).on ('pageshow', function () {
+	$("#confirmationPopup").popup ({ tolerance: "0px, 12px, 32px, 12px" });
 	$("#confirmationPopup").popup ("open");
 	
 	setTimeout (
 		function () {
 			$("#confirmationPopup").popup ("close");
 		}, 
-		2000
+		3000
 	);
-	
-	$(".ui-popup-container.ui-popup-active").css ("max-width", "inherit");
-	$(".ui-popup-container.ui-popup-active").css ("top", "88px");
-	$(".ui-popup-container.ui-popup-active").css ("bottom", "100px");
-	$(".ui-popup-container.ui-popup-active").css ("right", "12px");
-	$(".ui-popup-container.ui-popup-active").css ("left", "12px");
 });
