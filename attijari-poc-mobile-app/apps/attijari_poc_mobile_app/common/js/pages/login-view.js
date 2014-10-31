213c636d3a9e9837baceb352ce1435540f6527eb
Mehdi <mehdiben@ma.ibm.com>
2014-10-31 16:25:13 +0000

    Update login-view.js

--- attijari-poc-mobile-app/apps/attijari_poc_mobile_app/common/js/pages/login-view.js
+++ attijari-poc-mobile-app/apps/attijari_poc_mobile_app/common/js/pages/login-view.js
@@ -1,52 +1,113 @@
-$( document ).ready(function() {
-	$("#dialog").hide ();	
-	$("#dialog").click (function () {
-        $("#dialog").hide ();
-    });
+var language = "french";
+
+$(document).on ("pageinit", function () {
 	
-	if (connected == 1) {
-		loadPage("apps-list.html");		
-	}	
+	$(".language-actions .ui-block-b a").on ("click", function () {
+		if (language == "arabic") {
+			changeStyle (
+				$(this), 
+				$(".language-actions .ui-block-a a"),
+				false
+			);
+			language =  "french";
+			
+			$(".loading-language").css ("display", "block");
+			$(".loader").css ("display", "block");
+			$(".authentication-error").css ("display", "none");
+			
+			displayRedPopup (false, true);
+		} 
+	});
+	
+	$(".language-actions .ui-block-a a").on ("click", function () {
+		if (language == "french") {
+			changeStyle (
+				$(this), 
+				$(".language-actions .ui-block-b a")
+			);
+			language =  "arabic";
+			
+			$(".loading-language").css ("display", "block");
+			$(".loader").css ("display", "block");
+			$(".authentication-error").css ("display", "none");
+			
+			displayRedPopup (false, true);
+		} 
+	});
+	
+	$(".submit-login").on ("click", function () {
+		testData ();
+	});
 });
 
-$("#forgotpass").on ("click",function () {
-	loadPage ("forgot-password-view.html");
-});
+function changeStyle (focusBlock, freeBlock) {
+	focusBlock.css ("color", "#FFF");
+	focusBlock.css ("background-color", "#ef5a49");
+	focusBlock.css ("border-color", "#ef5a49");
+	
+	freeBlock.css ("color", "#ef5a49");
+	freeBlock.css ("background-color", "#FFF");
+	freeBlock.css ("border-color", "#ef5a49");
+}
 
-$("#registerlink").on ("click",function () {
-	loadPage ("register-view.html");
-});
+function displayRedPopup (dismissibleBool, translateBool) {
+	$("#confirmationPopup").attr ("data-dismissible", dismissibleBool);
+	
+	$("#confirmationPopup").popup ({ tolerance: "0px, 12px, 32px, 12px" });
+	$("#confirmationPopup").popup ("open");
+		
+	setTimeout (
+		function () {
+			$("#confirmationPopup").popup ("close");
+		}, 
+		4000
+	);
+	
+	$(document).bind ({
+		popupafterclose: function (event, ui) {
+			if (translateBool) {
+				console.log ("yes translate");
+				translate ();
+			}
+		}
+	});
+}
 
-$("#frensh").on ("click",function () {
-	$("#buttongo").html("Se connecter");
-	$("#registerlink").html("S'enregister");
-	$("#forgotpass").html("Mot de passe oulbié!");
-	$("#passwordinput").attr("placeholder", "Mot de passe");
-	$("#emailinput").attr("placeholder", "Email");
-	
-	
-	
-});
-$("#arabic").on ("click",function () {
-	$("#buttongo").html("تسجيل الدخول");
-	$("#registerlink").html("التسجيل");
-	$("#forgotpass").html("إستعادة كلمة السر");
-	$("#emailinput").attr("placeholder","البريد");
-	$("#passwordinput").attr("placeholder","كلمة السر");
-	
-});
-
-
-$("#buttongo").on("click",function(){
-	if($('#emailinput').val () == "demo" && $('#passwordinput').val () == "demo") {
-		connected = 1;
-	
-		//loadPage("pages/home-view.html");
-		loadPage ("home-view.html");	
+function translate () {
+	// translation
+	if (language == "french") {
+		$(".email-p input").attr ("placeholder", "Addresse E-mail");
+		$(".pass-p input").attr ("placeholder", "Mot de passe");
+		$(".submit-login button").text ("Connexion");
+		$(".links a.left").text ("Mot de passe oublié ?");
+		
+		$(".email-p input").css ("text-align", "left");
+		$(".pass-p input").css ("text-align", "left");
 	} else {
-		$("#dialog").dialog ();
-		$("#dialog").show ();
+		$(".email-p input").attr ("placeholder","البريد");
+		$(".pass-p input").attr ("placeholder","كلمة السر");
+		$(".submit-login button").text ("دخول");
+		$(".links a.left").text ("إستعادة كلمة السر");
+		
+		$(".email-p input").css ("text-align", "right");
+		$(".pass-p input").css ("text-align", "right");
 	}
-}); 
+}
 
-
+function testData () {
+	
+	if ($.trim ($(".email-p input").val ()) === '' || 
+			$.trim ($(".pass-p input").val ()) === '' || 
+			$.trim ($(".email-p input").val ()) !== 'demo' || 
+			$.trim ($(".pass-p input").val ()) !== 'demo') {
+		
+		$(".loading-language").css ("display", "none");
+		$(".loader").css ("display", "none");
+		$(".authentication-error").css ("display", "block");
+		
+		displayRedPopup (true, false);
+	} else {
+		connected = 1;
+		loadPage ("home-view.html");
+	}
+}
