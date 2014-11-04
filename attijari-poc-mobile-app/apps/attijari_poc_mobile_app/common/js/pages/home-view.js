359520fa3c7dfac6f0d9dc2d45eafebe5e67806c
next1 <h_maazouz@hotmail.com>
2014-11-04 15:30:21 +0000

    home view :fixing timer interval

--- attijari-poc-mobile-app/apps/attijari_poc_mobile_app/common/js/pages/home-view.js
+++ attijari-poc-mobile-app/apps/attijari_poc_mobile_app/common/js/pages/home-view.js
@@ -20,32 +20,32 @@ var globalarray=[];
 $( document ).ready(function() {
 	var options = {
 			$AutoPlay: "false",
-			$AutoPlayInterval:3000,
+			$AutoPlayInterval:2000,
             $Loop: 0,            
             $DragOrientation: 1,                               
         };
 	var options1 = {
 			$Transitions :{$Duration:1200,x:-0.3,y:0.3,$Delay:60,$Zoom:1,$Formation:$JssorSlideshowFormations$.$FormationStraightStairs,$Easing:{$Left:$JssorEasing$.$EaseInJump,$Top:$JssorEasing$.$EaseInJump,$Opacity:$JssorEasing$.$EaseLinear},$Opacity:2,$Round:{$Left:0.8,$Top:0.8}},
-			$AutoPlayInterval:3000,
-			$AutoPlay: true,
+			$AutoPlayInterval:2000,
+			$AutoPlay: false,
 			$DragOrientation: 3,                               
 	};
 	var options2 = {
 			$Transitions :{$Duration:1200,x:-0.3,y:0.3,$Delay:60,$Zoom:1,$Formation:$JssorSlideshowFormations$.$FormationStraightStairs,$Easing:{$Left:$JssorEasing$.$EaseInJump,$Top:$JssorEasing$.$EaseInJump,$Opacity:$JssorEasing$.$EaseLinear},$Opacity:2,$Round:{$Left:0.8,$Top:0.8}},
-			$AutoPlayInterval: 3000,
-			$AutoPlay: true,
+			$AutoPlayInterval: 2000,
+			$AutoPlay: false,
 			$DragOrientation: 3,                               
 	};
 	var options3 = {
 			$Transitions :{$Duration:1200,x:-0.3,y:0.3,$Delay:60,$Zoom:1,$Formation:$JssorSlideshowFormations$.$FormationStraightStairs,$Easing:{$Left:$JssorEasing$.$EaseInJump,$Top:$JssorEasing$.$EaseInJump,$Opacity:$JssorEasing$.$EaseLinear},$Opacity:2,$Round:{$Left:0.8,$Top:0.8}},
-			$AutoPlayInterval: 3000,
-			$AutoPlay: true,
+			$AutoPlayInterval: 2000,
+			$AutoPlay: false,
 			$DragOrientation: 3,                               
 	};
 	var options4 = {
 			$Transitions :{$Duration:1200,x:-0.3,y:0.3,$Delay:60,$Zoom:1,$Formation:$JssorSlideshowFormations$.$FormationStraightStairs,$Easing:{$Left:$JssorEasing$.$EaseInJump,$Top:$JssorEasing$.$EaseInJump,$Opacity:$JssorEasing$.$EaseLinear},$Opacity:2,$Round:{$Left:0.8,$Top:0.8}},
-			$AutoPlayInterval: 3000,
-            $AutoPlay: true,
+			$AutoPlayInterval: 2000,
+            $AutoPlay: false,
             $DragOrientation: 3,                               
         };
 
@@ -67,22 +67,20 @@ $( document ).ready(function() {
 		globalarray[2]=jssor_slider3 ;
 		globalarray[3]=jssor_slider4 ;
 		globalarray[4]=jssor_slider5 ;
-		setInterval("tik()",1000);
+		setInterval("tik()",3000);
 
 		
 	
 });
 function tik() {
 	var j = Math.floor((Math.random() * 4) + 0);
-	
+	console.log(globalarray.length-1);
 	for (var i=0;i<globalarray.length;i++){
 		if (j==i){
-		
+			console.log(i);
 			 globalarray[i].$Play();
 		}
-		else{
-			 globalarray[i].$Pause();
-		}
+		else {globalarray[i].$Pause();}
 	};
 	
 };
