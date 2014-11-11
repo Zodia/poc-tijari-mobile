f7f60f3ebf66f669818156417f657a444eec1a15
Mehdi <mehdiben@ma.ibm.com>
2014-11-11 16:16:24 +0000

    Update mortgage-center-view.js

--- attijari-poc-mobile-app/apps/attijari_poc_mobile_app/common/js/pages/mortgage-center-view.js
+++ attijari-poc-mobile-app/apps/attijari_poc_mobile_app/common/js/pages/mortgage-center-view.js
@@ -1,78 +1,68 @@
-$( document ).ready(function() {
-	var options = {
-			$AutoPlay: "false",
-			$AutoPlayInterval: 10000,
-            $Loop: 0,            
-            $DragOrientation: 3, 
-            $BulletNavigatorOptions: {                                //[Optional] Options to specify and enable navigator or not
-                $Class: $JssorBulletNavigator$,                       //[Required] Class to create navigator instance
-                $ChanceToShow: 2,                               //[Required] 0 Never, 1 Mouse Over, 2 Always
-                $AutoCenter: 1,                                 //[Optional] Auto center navigator in parent container, 0 None, 1 Horizontal, 2 Vertical, 3 Both, default value is 0
-                $Steps: 1,                                      //[Optional] Steps to go for each navigation request, default value is 1
-                $Lanes: 1,                                      //[Optional] Specify lanes to arrange items, default value is 1
-                $SpacingX: 8,                                   //[Optional] Horizontal space between each item in pixel, default value is 0
-                $SpacingY: 8,                                   //[Optional] Vertical space between each item in pixel, default value is 0
-                $Orientation: 1                                 //[Optional] The orientation of the navigator, 1 horizontal, 2 vertical, default value is 1
-            }
-        };
-	var options1 = {
-			$AutoPlayInterval: 5000,
-			$AutoPlay: true,
-			$DragOrientation: 3                               
-	};
-	var options2 = {
-			$AutoPlayInterval: 7000,
-			$AutoPlay: true,
-			$DragOrientation: 3                               
-	};
-	var options3 = {
-			$AutoPlayInterval: 4500,
-			$AutoPlay: true,
-			$DragOrientation: 3                               
-	};
-	var options4 = {
-			$AutoPlayInterval: 6500,
-			$AutoPlay: true,
-			$DragOrientation: 3                              
-	};
+$(document).on ("pageshow", function() {
 	
+	$(".applications").on ("click", function() {
+		loadPage("applications-search-view.html");
+	});
 
-		var jssor_slider6 = new $JssorSlider$("slideshow5", options);
-		var jssor_slider7 = new $JssorSlider$("slideshow6", options1);
-		var jssor_slider8 = new $JssorSlider$("slideshow7", options2);
-		var jssor_slider9 = new $JssorSlider$("slideshow8", options3);
-		var jssor_slider10 = new $JssorSlider$("slideshow9", options4);
+	$(".new-mortgage").on ('click', function() {
+		confirmDialog("Nouveau Client?", function() {
+			customer = {};
+			property = {};
+			loadPage("customer-view.html");
+		}, function() {
+			loadPage("customers-search-view.html");
+		});
+	});
 
-		
-   
-        jssor_slider6.$Play();
-	
-});
-
-
-
-
-$(".applications").click(function() {
-	loadPage("applications-search-view.html");
-});
-
-$(".new-mortgage").on('click', function() {
-	confirmDialog("Nouveau Client?", function() {
-		customer = {};
-		property = {};
-		loadPage("customer-view.html");
-	}, function() {
-		loadPage("customers-search-view.html");
+	$(".mortgage-simulator").on ("click", function() {
+		loadPage ("mortgage-simulator-view.html");
 	});
 });
 
-$(".mortgage-simulator").click(function() {
-	loadPage("mortgage-simulator-view.html");
-});
+var options = {
+		$AutoPlay: "false",
+		$AutoPlayInterval: 10000,
+        $Loop: 0,            
+        $DragOrientation: 3, 
+        $BulletNavigatorOptions: {                                //[Optional] Options to specify and enable navigator or not
+            $Class: $JssorBulletNavigator$,                       //[Required] Class to create navigator instance
+            $ChanceToShow: 2,                               //[Required] 0 Never, 1 Mouse Over, 2 Always
+            $AutoCenter: 1,                                 //[Optional] Auto center navigator in parent container, 0 None, 1 Horizontal, 2 Vertical, 3 Both, default value is 0
+            $Steps: 1,                                      //[Optional] Steps to go for each navigation request, default value is 1
+            $Lanes: 1,                                      //[Optional] Specify lanes to arrange items, default value is 1
+            $SpacingX: 8,                                   //[Optional] Horizontal space between each item in pixel, default value is 0
+            $SpacingY: 8,                                   //[Optional] Vertical space between each item in pixel, default value is 0
+            $Orientation: 1                                 //[Optional] The orientation of the navigator, 1 horizontal, 2 vertical, default value is 1
+        }
+};
+var options1 = {
+		$AutoPlayInterval: 5000,
+		$AutoPlay: true,
+		$DragOrientation: 3                               
+};
+var options2 = {
+		$AutoPlayInterval: 7000,
+		$AutoPlay: true,
+		$DragOrientation: 3                               
+};
+var options3 = {
+		$AutoPlayInterval: 4500,
+		$AutoPlay: true,
+		$DragOrientation: 3                               
+};
+var options4 = {
+		$AutoPlayInterval: 6500,
+		$AutoPlay: true,
+		$DragOrientation: 3                              
+};
 
-$(".refinancing").click(function() {
-	// loadPage ("mortgage-center-view.html");
-});
+var jssor_slider6 = new $JssorSlider$("slideshow5", options);
+var jssor_slider7 = new $JssorSlider$("slideshow6", options1);
+var jssor_slider8 = new $JssorSlider$("slideshow7", options2);
+var jssor_slider9 = new $JssorSlider$("slideshow8", options3);
+var jssor_slider10 = new $JssorSlider$("slideshow9", options4);
+
+jssor_slider6.$Play();
 
 function confirmDialog(text, callback_New, callback_Existing) {
 	var popupDialogId = 'popupDialog';
