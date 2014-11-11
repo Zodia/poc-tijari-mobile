44ccbc79c21254d0bbba535c61b92e8fdd202699
Mehdi <mehdiben@ma.ibm.com>
2014-11-11 16:18:56 +0000

    Update notifications-center-view.js

--- attijari-poc-mobile-app/apps/attijari_poc_mobile_app/common/js/pages/notifications-center-view.js
+++ attijari-poc-mobile-app/apps/attijari_poc_mobile_app/common/js/pages/notifications-center-view.js
@@ -1,47 +1,11 @@
-/*$(function() {
-
-    function prevent_default (e) {
-        e.preventDefault ();
-    }
-
-    function disable_scroll () {
-        $(document).on ('touchmove', prevent_default);
-    }
-
-    function enable_scroll () {
-        $(document).unbind ('touchmove', prevent_default)
-    }
-
-    var x;
-    $('.notifications-list li > a')
-	    .on ('touchstart', function(e) {
-	        $('.notifications-list li > a').css ('left', '0px') // close em all
-	        x = e.originalEvent.pageX // anchor point
-	    })
-	    .on ('touchmove', function(e) {
-	        var change = e.originalEvent.pageX - x
-	        change = Math.min (Math.max (-100, change), 0) // restrict to -100px left, 0px right
-	        e.currentTarget.style.left = change + 'px'
-	        if (change < -10) {
-	        	disable_scroll (); // disable scroll once we hit 10px horizontal slide
-	        }
-	    })
-        .on ('touchend', function(e) {
-            var left = parseInt (e.currentTarget.style.left)
-            var new_left = (left > -50 ? '0px' : '-100px') // snap back, or leave open, 50px threshold
-            e.currentTarget.style.left = new_left
-            enable_scroll ()
-        });
-
-    $('li .delete-btn').on ('touchend', function (e) {
-        e.preventDefault ()
-        $(this).parents ('li').slideUp ('fast', function () {
-            $(this).remove ()
-        })
-    })
-});*/
-
 $(document).on ("pageshow", function () {
+	
+	var listItemId = "listItemId-0";
+	var pic = "";
+	var title = "";
+	var extract = "";
+	var content = "";
+	
 	var currentAllNotifications = 0;
 
 	$("#unread-notifications").addClass ($.mobile.activeBtnClass);
@@ -56,11 +20,6 @@ $(document).on ("pageshow", function () {
 	});
 
 	function fillList (count, append, all) {
-		var listItemId = "listItemId-0";
-		var pic = "";
-		var title = "";
-		var extract = "";
-		var content = "";
 		
 		if (!$(this).hasClass ($.mobile.activeBtnClass)) {
 			
